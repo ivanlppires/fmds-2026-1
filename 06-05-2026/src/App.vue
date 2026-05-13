e377a1fa229859dedeca50f31e60208518d92a7b
Ivan Pires <ivanpires@gmail.com>
2026-05-13 19:21:41 -0400

    inicio

---
+++ 06-05-2026/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue'
+const teste = () => {
+  window.alert('Olá, mundo!')
+}
+
+</script>
+<template>
+  <button @click="teste">clique em mim</button>
+</template>
