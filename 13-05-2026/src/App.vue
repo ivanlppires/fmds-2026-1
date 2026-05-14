fea5dc02705c9bc17151016b965a74601e20bf9a
Ivan Pires <ivanpires@gmail.com>
2026-05-13 21:15:08 -0400

    Exemplo pra ajudar

--- 13-05-2026/src/App.vue
+++ 13-05-2026/src/App.vue
@@ -1,30 +1,28 @@
 <template>
-  <div v-show="loggedIn">
-    <h1 style="border-bottom:2px dotted blue">Sistema do Ivan</h1>
-  </div>
-  <div v-if="loggedIn">
-    <p>Seja bem vindo ao meu site!</p>
-    <input v-model="newItem">
+    <h1>Quitanda do Ivan</h1>
+    <hr>
+    <input type="text" v-model="newItem">
     <button @click="addItem">Adicionar Item</button>
-
-    <!-- v-for -->
-     <p v-for="(item, index) in items" :key="index"> - {{  item }}</p>
-
-    <button @click="loggedIn = false">Sair</button>
-  </div>
-  <div v-else>
-    <input placeholder="usuario">
-    <input placeholder="senha" type="password">
-    <button @click="loggedIn = true">Login</button>
-  </div>
+    <hr>
+    <ul>
+        <li v-for="(item, index) in items" :key="index">
+            {{ item }}
+            <button @click="removeItem(index)">Remover</button>
+        </li>
+    </ul>
 </template>
 
 <script setup>
 import { ref } from 'vue';
-const loggedIn = ref(true);
-const items = ref(['Item 1', 'Item 2', 'Item 3']);
 const newItem = ref('');
-const addItem = () => {
-  items.value.push(newItem.value);
+const items = ref([]);
+function addItem() {
+    if (newItem.value.trim() !== '') {
+        items.value.push(newItem.value);
+        newItem.value = '';
+    }
+}
+function removeItem(index) {
+    items.value.splice(index, 1);
 }
 </script>
---
+++ 13-05-2026/src/App.vue.1
@@ -0,0 +1,30 @@
+<template>
+  <div v-show="loggedIn">
+    <h1 style="border-bottom:2px dotted blue">Sistema do Ivan</h1>
+  </div>
+  <div v-if="loggedIn">
+    <p>Seja bem vindo ao meu site!</p>
+    <input v-model="newItem">
+    <button @click="addItem">Adicionar Item</button>
+
+    <!-- v-for -->
+     <p v-for="(item, index) in items" :key="index"> - {{  item }}</p>
+
+    <button @click="loggedIn = false">Sair</button>
+  </div>
+  <div v-else>
+    <input placeholder="usuario">
+    <input placeholder="senha" type="password">
+    <button @click="loggedIn = true">Login</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const loggedIn = ref(true);
+const items = ref(['Item 1', 'Item 2', 'Item 3']);
+const newItem = ref('');
+const addItem = () => {
+  items.value.push(newItem.value);
+}
+</script>
